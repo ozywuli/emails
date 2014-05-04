08aa732495a61181d379a8be9e32d2eec2f969b1
Ozy Wu-Li <ozy.wu.li@gmail.com>
2014-05-03 20:33:05 -0700

    gallery/ink/ink.html

--- 404.php
+++ 404.php
@@ -2,4 +2,8 @@
 
 <?php require( 'templates/header.php' ); ?>
 
+<h2>404</h2>
+
+<p>Oops, the page you were looking for cannot be found. Please try again or head back to the <a href="index.php">home page</a>.</p>
+
 <?php require( 'templates/footer.php' ); ?>
