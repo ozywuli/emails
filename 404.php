30db1c983040015760e73beb50ade9defca8284f
Ozy Wu-Li <ozy.wu.li@gmail.com>
2014-05-03 20:35:57 -0700

    Add 404 page

--- 404.php
+++ 404.php
@@ -1,9 +1,13 @@
 <?php $title = '404' ?>
+<?php
+  require( 'config.php' );
+  require( TEMPLATES_PATH . '/header.php' );
+?>
 
-<?php require( 'templates/header.php' ); ?>
-
+<div class="content-container">
 <h2>404</h2>
 
 <p>Oops, the page you were looking for cannot be found. Please try again or head back to the <a href="index.php">home page</a>.</p>
+</div>
 
-<?php require( 'templates/footer.php' ); ?>
+<?php require( TEMPLATES_PATH . '/footer.php' ); ?>
